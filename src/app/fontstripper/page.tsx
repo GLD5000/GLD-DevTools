b41e89b2d8725d303b357419273dd4b41b33fc85
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-17 17:08:45 +0100

    Added broken fontstripper

---
+++ src/app/fontstripper/page.tsx
@@ -0,0 +1,12 @@
+import Form from "./Form";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="my-10 mx-auto w-fit text-6xl font-bold">
+        Font Character Stripping
+      </h1>
+      <Form />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -5,12 +7,10 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           GLD API Hub
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-    
-        </div>
+        {/* <Link href={"/fontstripper"}>
+      font stripper
+     </Link> */}
       </div>
-
-
     </main>
-  )
+  );
 }
