6e7ac1ed7694118705f6cec92ede4fc44fbdc536
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-19 12:00:35 +0100

    Updated client info API

--- src/app/api/client/route.ts
+++ src/app/api/client/route.ts
@@ -3,10 +3,12 @@ import { NextRequest } from "next/server";
 /* eslint-disable import/prefer-default-export */
 async function handler(request: NextRequest) {
   if (request.method === "GET") {
-    const ipaddress = request.ip || "IP Not Found";
     const requestHeaders = new Headers(request.headers);
     const language = requestHeaders.get("accept-language");
     const software = requestHeaders.get("user-agent");
+    const ipaddress =
+      request.ip || requestHeaders.get("x-forwarded-for")?.split(",")[0];
+
     return new Response(
       JSON.stringify({
         ipaddress,
