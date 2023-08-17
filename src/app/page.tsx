d5f436ec24eb3b525098de6491890e53ad05b92c
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-17 23:37:59 +0100

    Update justify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Parsers from "@/markdown/parsers/allParsers.mdx";
 
 export default function Home() {
   return (
-    <main className="grid w-screen h-screen gap-20 items-center justify-between p-24 smooth-scroll">
-      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert mx-auto px-2">
+    <main className="grid w-full justify-center h-screen gap-20 items-center p-24 smooth-scroll">
+      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert px-2">
         <Tutorials />
       </div>
-      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert mx-auto px-2">
+      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert px-2">
         <Apis />
       </div>
-      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert mx-auto px-2">
+      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert px-2">
         <Parsers />
       </div>
     </main>
