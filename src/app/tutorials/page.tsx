77e0cb09f3d7e359405ed5268f1af2552a881282
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-17 17:01:33 +0100

    Add prose config

--- src/app/tutorials/page.tsx
+++ src/app/tutorials/page.tsx
@@ -2,7 +2,7 @@ import HtmlMd from "@/markdown/tutorials/html.mdx";
 
 export default function page() {
   return (
-    <div className="w-full max-w-[1400px] mx-auto sm:px-20">
+    <div className="w-full max-w-[1400px] mx-auto sm:px-20 prose-lg">
       <HtmlMd />
     </div>
   );
