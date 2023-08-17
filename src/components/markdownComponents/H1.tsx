7704d8ca1de355a4d018c087ebe5908e2127fb8e
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-17 22:46:03 +0100

    Add searches page

--- src/components/markdownComponents/H1.tsx
+++ src/components/markdownComponents/H1.tsx
@@ -4,7 +4,7 @@ export default function H1({ children }: { children: ReactNode }) {
   return (
     <h1
       id="top"
-      className="w-fit mx-auto text:4xl sm:text-6xl xl:text-7xl font-bold"
+      className="w-fit mx-auto text:4xl sm:text-6xl xl:text-7xl font-bold text-center"
     >
       {children}
     </h1>
