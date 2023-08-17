41873408eb1f79b2b777fe20172754d181ec7de4
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-17 16:14:39 +0100

    Add mdx tutorials page

--- src/app/(markdown)/tutorials/page.tsx
+++
@@ -1,5 +0,0 @@
-export default async function page() {
-  const content = await fetch("http://localhost:3000/tutorials/html.md");
-  const text = content.text();
-  return <div>{text}</div>;
-}
---
+++ src/app/tutorials/page.tsx
@@ -0,0 +1,5 @@
+import HtmlMd from "@/markdown/tutorials/html.mdx";
+
+export default function page() {
+  return <HtmlMd />;
+}
