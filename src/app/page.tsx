1187fa16a9e73f4bbc189b8dbcf4ddb0f6bc6976
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-17 23:36:18 +0100

    Update Width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Parsers from "@/markdown/parsers/allParsers.mdx";
 
 export default function Home() {
   return (
-    <main className="grid h-screen gap-20 items-center justify-between p-24 smooth-scroll">
+    <main className="grid w-screen h-screen gap-20 items-center justify-between p-24 smooth-scroll">
       <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert mx-auto px-2">
         <Tutorials />
       </div>
