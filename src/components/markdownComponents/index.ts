71d0c6f28c54245d0a7f0a661512a5d7a0816073
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-18 08:36:50 +0100

    Add H4

--- src/components/markdownComponents/index.ts
+++ src/components/markdownComponents/index.ts
@@ -2,7 +2,8 @@
 import H1 from "./H1";
 import H2 from "./H2";
 import H3 from "./H3";
+import H4 from "./H4";
 import A from "./A";
 import Code from "./Code";
 
-export { H1, H2, H3, A, Code };
+export { H1, H2, H3, H4, A, Code };
