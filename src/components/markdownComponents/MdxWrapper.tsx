efeaf8b006f4a62c3a3cf30ea191ebac08eaf25c
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-18 09:00:14 +0100

    Add Template links

---
+++ src/components/markdownComponents/MdxWrapper.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function MdxWrapper({ children }: { children: ReactNode }) {
+  return (
+    <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert px-2">
+      {children}
+    </div>
+  );
+}
