1d2484d144c4bc8d58abf7edf9bbaf732b1ba6d0
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-19 09:54:21 +0100

    Add design patterns

---
+++ src/app/(tutorials)/designpatterns/page.tsx
@@ -0,0 +1,14 @@
+import MarkDown from "@/markdown/tutorials/designpatterns.mdx";
+
+export default function page() {
+  return (
+    <main
+      id="main"
+      className="w-screen h-screen overflow-y-scroll smooth-scroll-gld"
+    >
+      <div className="w-full h-fit max-w-[1000px] sm:px-8 xl:px-0 prose prose-base sm:prose-lg lg:prose-xl prose-invert mx-auto px-2">
+        <MarkDown />
+      </div>
+    </main>
+  );
+}
