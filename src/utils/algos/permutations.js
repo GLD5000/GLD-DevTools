afb07fdd1e8e2742242fb8924706f420bbfa43fc
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-20 13:53:27 +0100

    Add Heap permutations

---
+++ src/utils/algos/permutations.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-param-reassign */
+function heapPermutation(data, n) {
+  if (n === 1) {
+    console.log("Return Row", data);
+    return;
+  }
+
+  for (let i = 0; i < n; i = +1) {
+    console.log("heapPermutation(data, n - 1):", data, n - 1);
+    heapPermutation(data, n - 1);
+    if (n % 2 === 0) {
+      console.log("even swap data[i], data[n - 1]", i, n - 1);
+      [data[i], data[n - 1]] = [data[n - 1], data[i]]; // Swap elements
+    } else {
+      console.log("odd swap data[0], data[n - 1]", 0, n - 1);
+      [data[0], data[n - 1]] = [data[n - 1], data[0]]; // Swap elements
+    }
+  }
+}
+
+const data = [1, 2, 3];
+console.log("heapPermutation Start", data, data.length);
+heapPermutation(data, data.length);
