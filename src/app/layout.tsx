fd15e043eeda0aa02120965d7d04d36f615df341
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-28 12:50:00 +0100

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "GLD DevTools",
   description: "A collection of useful APIs and utilities for Web Devs",
+  icons: { icon: "/gldcondensed.ico" },
+  keywords:
+    "Blog, React, web, coding, developer, HTML, CSS, JavaScript, TypeScript",
+  authors: [{ name: "GLD", url: "https://github.com/GLD5000" }],
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
