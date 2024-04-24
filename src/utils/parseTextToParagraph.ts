caaff752ab114aaefe257ba9f160d403859639ba
Gareth <33987109+GLD5000@users.noreply.github.com>
2024-04-24 14:06:19 +0100

    Update parseTextToParagraph.ts

--- src/utils/parseTextToParagraph.ts
+++ src/utils/parseTextToParagraph.ts
@@ -1,10 +1,8 @@
 export default function textToParagraphArray(textIn: string) {
   const quotationMarks = `","`;
-  const fullStop = ".";
   const invisibleCharacter = " ";
   return `["${textIn
     .trim() //
     .replaceAll(invisibleCharacter, "")
-    .replaceAll(/(\. +)/g, fullStop)
     .replaceAll(/( *[\r\n]{1,} *)+/g, quotationMarks)}"]`;
 }
