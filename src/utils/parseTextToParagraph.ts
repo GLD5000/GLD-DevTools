8ede800d69541eda111bd8a0e4e1228bef57b743
Gareth <33987109+GLD5000@users.noreply.github.com>
2024-04-24 11:31:45 +0000

    Remove invisible char

--- src/utils/parseTextToParagraph.ts
+++ src/utils/parseTextToParagraph.ts
@@ -1,8 +1,10 @@
 export default function textToParagraphArray(textIn: string) {
   const quotationMarks = `","`;
   const fullStop = ".";
+  const invisibleCharacter = " ";
   return `["${textIn
-    .trim()
+    .trim() //
+    .replaceAll(invisibleCharacter, "")
     .replaceAll(/(\. +)/g, fullStop)
     .replaceAll(/( *[\r\n]{1,} *)+/g, quotationMarks)}"]`;
 }
