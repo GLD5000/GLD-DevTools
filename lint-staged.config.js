727f8c938d44a3a65c2daaef4f4c1789d1000766
Gareth Devlin <garethlouisdevlin@gmail.com>
2024-07-10 15:55:12 +0100

    Upgrade Next.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // this will check Typescript files
-  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+  "**/*.(ts|tsx)": () => "npx tsc --noEmit",
 
   // This will format and lint TS & JS Files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn prettier --write ${filenames.join(" ")}`,
-    `yarn eslint --fix ${filenames.join(" ")}`,
+    `npx prettier --write ${filenames.join(" ")}`,
+    `npx eslint --fix ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
   "**/*.(md|json)": (filenames) =>
-    `yarn prettier --write ${filenames.join(" ")}`,
+    `npx prettier --write ${filenames.join(" ")}`,
 };
